a9b7b0442cdd317f3721ef3be06148a75644bf30
kajankowska <85677666+kajankowska@users.noreply.github.com>
2021-09-15 00:17:00 +0200

    dict added

--- meal_plan.py
+++ meal_plan.py
@@ -9,35 +9,29 @@ with open("key.txt") as file:
 
 exc = "Drink, Mojito, Wine, Vodka, Cocktail, Rum, Whisky, Lemonade, Dip, Cake"
 
-with open("meals.json", "w") as file:
-    file.write("")
+url = "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/mealplans/generate"
 
-    url = "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/mealplans/generate"
+querystring = {"timeFrame": "week", "targetCalories": "2400", "diet": "vegetarian",
+                "exclude": exc}
 
-    querystring = {"timeFrame": "week", "targetCalories": "2400", "diet": "vegetarian",
-                   "exclude": exc}
+headers = {
+    'x-rapidapi-host': "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
+    'x-rapidapi-key': key
+}
 
-    headers = {
-        'x-rapidapi-host': "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
-        'x-rapidapi-key': key
-    }
-
-    response = requests.request("GET", url, headers=headers, params=querystring)
-    rowdata = response.json()
+response = requests.request("GET", url, headers=headers, params=querystring)
+rowdata = response.json()
 
 with open("dane.csv", "w", newline="") as file:
     writer = csv.writer(file)
-    weekly_menu_data = []
     for line in rowdata["items"]:
         meal = line["value"]
-        mealplanID = line["mealPlanId"]
+        mealplanid = line["mealPlanId"]
         slot = line["slot"]
         day = line["day"]
         ast.literal_eval
         meal_dict = ast.literal_eval(line["value"])
         meal_id = meal_dict["id"]
         meal_title = meal_dict["title"]
-        dish_menu_data = [mealplanID, day, slot, meal_title]
+        dish_menu_data = [mealplanid, day, slot, meal_title]
         meals[meal_id] = dish_menu_data
-        weekly_menu_data.append(dish_menu_data)
-    writer.writerows(weekly_menu_data)
