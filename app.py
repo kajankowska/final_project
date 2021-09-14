fa5a5374b8dab8cc38177ad55656b5a5c1b6db51
kajankowska <85677666+kajankowska@users.noreply.github.com>
2021-09-14 15:52:30 +0200

    routing

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
 from flask import Flask, render_template
 
-with open("key.txt") as file:
-    key = file.readlines()[0]
 
 app = Flask(__name__)
 
@@ -9,6 +7,32 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return render_template('index.html')
+
+
+@app.route('/register/')
+def form():
+    return render_template('register.html')
+
+
+@app.route('/options/')
+def fill():
+    return render_template('options.html')
+
+
+@app.route('/generator/')
+def processing():
+    return render_template('plan_generator.html')
+
+
+@app.route('/mealplan/')
+def result():
+    return render_template('meal_plan.html')
+
+
+@app.route('/endofreg/')
+def regend():
+    return render_template('end_of_register.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
