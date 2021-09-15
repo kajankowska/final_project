4a01dd8beedc96c91a24ef3a4725b5d1f4676600
kajankowska <85677666+kajankowska@users.noreply.github.com>
2021-09-15 10:03:45 +0200

    update

--- nutritions.py
+++ nutritions.py
@@ -1,37 +1,33 @@
 import requests
 import csv
-import ast
+from meal_plan import meals
 
-meals = {}
+nutritions = {}
 
 with open("key.txt") as file:
     key = file.readlines()[0]
 
-exc = "Drink, Mojito, Wine, Vodka, Cocktail, Rum, Whisky, Lemonade, Dip, Cake"
-
-url = "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/mealplans/generate"
-
-querystring = {"timeFrame": "week", "targetCalories": "2400", "diet": "vegetarian",
-                "exclude": exc}
-
 headers = {
     'x-rapidapi-host': "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
     'x-rapidapi-key': key
-}
+    }
 
-response = requests.request("GET", url, headers=headers, params=querystring)
-rowdata = response.json()
+recipeid = meals.keys()
+for line in recipeid:
+    url = "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/{}/nutritionWidget.json".format(line)
+    response = requests.request("GET", url, headers=headers)
+    output = response.json()
+    nutritions[line] = output
 
-with open("dane.csv", "w", newline="") as file:
+
+with open("wartosciodzywcze.csv", "a", newline="") as file:
     writer = csv.writer(file)
-    for line in rowdata["items"]:
-        meal = line["value"]
-        mealplanid = line["mealPlanId"]
-        slot = line["slot"]
-        day = line["day"]
-        ast.literal_eval
-        meal_dict = ast.literal_eval(line["value"])
-        meal_id = meal_dict["id"]
-        meal_title = meal_dict["title"]
-        dish_menu_data = [mealplanid, day, slot, meal_title]
-        meals[meal_id] = dish_menu_data
+    for line in nutritions:
+        cal = nutritions[line]["calories"]
+        carb = nutritions[line]["carbs"]
+        fat = nutritions[line]["fat"]
+        protein = nutritions[line]["protein"]
+        nutri_list = [line, cal, carb, fat, protein]
+        writer.writerow(nutri_list)
+
+print(output)
