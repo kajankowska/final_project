59920aeb4402c4169d509714ef3afad544750386
kajankowska <85677666+kajankowska@users.noreply.github.com>
2021-09-16 23:53:14 +0200

    routs added

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+from meal_plan import mp, nut, im
 
 
 app = Flask(__name__)
@@ -14,27 +15,40 @@ def form():
     return render_template('register.html')
 
 
+@app.route('/endofreg/', methods=["POST", "GET"])
+def regend():
+    if request.method == "POST":
+        personal = dict(request.form)
+        userlogin = request.form['login']
+        userward = request.form['ward']
+        userwing = request.form['wing']
+    return render_template('end_of_register.html')
+
+
 @app.route('/options/', methods=["POST", "GET"])
 def fill():
-    if request.method == "POST":
-        print(dict(request.form))
     return render_template('options.html')
 
 
 @app.route('/generator/', methods=["POST", "GET"])
 def processing():
-    print(dict(request.form))
+    if request.method == "POST":
+        print(dict(request.form))
+        rowdata = mp.get_api_data(request.form["calories"], request.form["diet type"], request.form["exc"])
+        mp.dict_save(rowdata)
+        # output = nut.get_nutritions()
+        # nut.nutritions_save(output)
     return render_template('plan_generator.html')
 
 
 @app.route('/mealplan/')
 def result():
-    return render_template('meal_plan.html')
+    return render_template('meal_plan.html', mealplan=mp.meals)
 
 
-@app.route('/endofreg/')
-def regend():
-    return render_template('end_of_register.html')
+@app.route('/summary/', methods=["POST", "GET"])
+def theend():
+    return render_template('final.html', )
 
 
 if __name__ == '__main__':
